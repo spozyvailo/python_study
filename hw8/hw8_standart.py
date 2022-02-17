3af2e49afa4e91df470362f8ed541b99d8c4f956
spozyvailo <cthutq91@gmail.com>
2022-02-17 23:15:55 +0200

    hw 8_st task

---
+++ hw8/hw8_standart.py
@@ -0,0 +1,30 @@
+"""
+Task 1
+
+A simple function.
+
+Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie.
+The function should then print “My favorite movie is named {name}”.
+
+
+Task 2
+
+Creating a dictionary.
+
+Create a function called make_country, which takes in a country’s name and capital as parameters.
+Then create a dictionary from those two, with ‘name’ as a key and ‘capital’ as a parameter.
+Make the function print out the values of the dictionary to make sure that it works as intended.
+
+
+Task 3
+
+A simple calculator.
+
+Create a function called make_operation, which takes in a simple arithmetic operator as a first parameter
+(to keep things simple let it only be ‘+’, ‘-’ or ‘*’) and an arbitrary number of arguments (only numbers)
+as the second parameter. Then return the sum or product of all the numbers in the arbitrary parameter. For example:
+
+the call make_operation(‘+’, 7, 7, 2) should return 16
+the call make_operation(‘-’, 5, 5, -10, -20) should return 30
+the call make_operation(‘*’, 7, 6) should return 42
+"""
